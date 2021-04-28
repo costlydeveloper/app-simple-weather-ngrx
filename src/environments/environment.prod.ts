ceb6b704a91c5cc3e874199842a6323977aae60f
CostlyDeveloper <55545296+CostlyDeveloper@users.noreply.github.com>
2021-04-29 00:42:58 +0200

    Change - https / api call

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  openWeatherURL: 'http://api.openweathermap.org/data/2.5',
+  openWeatherURL: 'https://api.openweathermap.org/data/2.5',
   openWeatherAPIKey: '1812c1f0b3182a81777d53bc120be2d9'
 };
